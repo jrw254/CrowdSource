bcea17336cd331a1fcee08deb6a05cb0ec965e49
JR <47221007+jrw254@users.noreply.github.com>
2022-01-12 22:20:26 -0600

    Add files via upload

---
+++ Javascript/PCPM.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Prolific Crude Page Monitor
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Page Monitor and Crudely scrape the page that can be displayed via Notifications
+// @author       JRW254
+// @match        https://app.prolific.co/studies
+// @icon         https://www.google.com/s2/favicons?domain=app.prolific.co
+// @grant        none
+// ==/UserScript==
+//
+// Below we see if the page has permissions granted so we can get those notifications. 
+console.log(Notification.permission);
+   if (Notification.permission === "granted") { // Do nothing if permissions are granted. 
+ //     alert("we have permission");
+} else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(permission => { // If permissions are not granted then make a request.
+         console.log(permission);
+   });
+}
+//
+// Now lets set our interval below:
+// PCMP = Prolific Crude Page Monitor for a function name. 
+setInterval(PCPM, 300000); // 5 minutes
+
+function PCPM() { // Function for both sets of notifcations.
+//
+const listItems = document.querySelectorAll(".list-item .study-info"); // This get's all the info but not in a great way.
+//
+let surNUM = document.querySelector(".list").getElementsByTagName("li").length; // Let us get the number of available survey's.
+const notification = new Notification("Prolific Page Monitor\n Survey's Available: " + surNUM, { // Create and display the notifications.
+    // icon: "yourimageurl.png"
+    icon: "https://www.google.com/s2/favicons?domain=app.prolific.co"
+})
+
+notification.onclick = function () { // If we click the first notification we will display all survey's in notification area.
+for (let i = 0; i < listItems.length; i++) { // Loop through.
+let bodyTEXT = listItems[i].textContent; // Store contents of Loop.
+const notification = new Notification("Crude PANDA\nAvailable: " + surNUM, { // Create and display the notifications.
+    body: " Details:\n" + bodyTEXT, // Crudely display info in the body.
+    // vibrate: true
+    // icon: "yourimageurl.png"
+   })
+  };
+ }
+}
+//
