a9465e6d1db1b82498d60050af8085a8a21b3a8a
JR <47221007+jrw254@users.noreply.github.com>
2022-07-07 10:45:47 -0500

    Add files via upload

---
+++ Javascript/Peppermint Page Monitor.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Peppermint Page Monitor
+// @namespace    
+// @version      1.0
+// @description  Appen page monitor for the project: Peppermint
+// @author       JRW254
+// @match        https://connect.appen.com/qrp/core/vendors/task/view/1159
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+// Allows us to set prmissions for toast notifications. Will display a pop up if they need to be granted, nothing if already granted. 
+console.log(Notification.permission);
+   if (Notification.permission === "granted") {
+ //     alert("we have permission");
+} else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(permission => {
+         console.log(permission);
+   });
+}
+
+function appenNotification() {
+    let taskDESC = document.querySelector(".task_table").querySelectorAll(".expand-description").length; // Grabs it via the Task Description tag
+    const notification = new Notification("Appen Pin's Page Monitor\n Work Available: " + taskDESC, {
+        icon: "https://www.google.com/s2/favicons?domain=https://connect.appen.com/"
+    })
+ }
+
+ function reFresh() { // Reloads the page
+    window.location.reload();
+}
+
+setTimeout(appenNotification, 15000); // 15 seconds after load sends notification
+setInterval(reFresh, 1200000); // 20 minutes to reload page
